2a7e6d2ace53aad28518d96e010ba676291801f1
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2023-02-17 15:33:37 +1100

    Add files via upload

---
+++ funni/nggyu.js
@@ -0,0 +1,28 @@
+// The following variable can be edited
+var fps = 1; // The frame rate of the animation, in fps
+
+// DO NOT EDIT THE STUFF BELOW THIS POINT
+var frameNumber = -1;
+var id = 0;
+var divs = document.getElementsByTagName("div");
+function start() {
+	window.divzero = divs[0];
+	for (var i = 0; i < divs.length; i++) {
+		console.log(divs[i].innerHTML.length);
+		divs[i].className = "hidden";
+	}
+	id = setInterval(animate, (1000/fps));
+	animate()
+}
+function animate() {
+	frameNumber++;
+	if (frameNumber < divs.length) {
+		for (var i = 0; i < divs.length; i++) {
+			divs[i].className = "hidden";
+		}
+		divs[frameNumber].className = "visible";
+	} else {
+		clearInterval(id);
+		location.reload();
+	}
+}
